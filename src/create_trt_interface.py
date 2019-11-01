940d557e497c9cfd11c94cb0e8af6ed0362f0ecd
Pawan Gogad <pg1099@txstate.edu>
2019-11-01 15:54:51 -0700

    Create create_trt_interface.py

---
+++ src/create_trt_interface.py
@@ -0,0 +1,3 @@
+import tensorflow.contrib.tensorrt as trt
+
+trt.create_inference_graph(input_saved_model_dir='/home/pawan/20180408-102900', output_saved_model_dir='/home/pawan/20180408-102900/trt')
